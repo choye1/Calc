ea5d8b05c010456b06b22eeb26eef6b2c7e86865
DaniilPrikhoddko <d1912dd@gmail.com>
2023-11-06 00:04:17 +0300

    введен алгоритм Дейкстры

--- Калькулятор/Калькулятор/Program.cs
+++ Калькулятор/Калькулятор/Program.cs
@@ -11,83 +11,149 @@ namespace калькулятор
         static void Main(string[] args)
         {
             string a = "";
-            string str = Console.ReadLine();
-            str = str.Replace(" ", "");
-            string operators = "+-*/";
+            string userStr = Console.ReadLine();
+            userStr = userStr.Replace(" ", "");
+            string operators = "+-*/^()";
             List<string> listNum = new List<string>();
             List<string> listOp = new List<string>();
-            for (int i = 0; i < str.Length; i++)
-            {
-                if (operators.Contains(str[i])) listOp.Add(str[i].ToString());
-            }
+            List<object> tokens = new List<object>();
+          
 
-            for (int i = 0; i < str.Length; i++)
+            for (int i = 0; i < userStr.Length; i++)
             {
-
-                if (Char.IsDigit(str[i]) && (i != (str.Length - 1)))
+                if (operators.Contains(userStr[i]))
                 {
-                    a += str[i].ToString();
+                    listOp.Add(userStr[i].ToString());    
                 }
-                else if (i == (str.Length - 1))
+                if (Char.IsDigit(userStr[i]) && (i != (userStr.Length - 1)))
                 {
-                    a += str[i].ToString();
+                    a += userStr[i].ToString();
+                }
+                else if (i == (userStr.Length - 1))
+                {
+                    a += userStr[i].ToString();
                     listNum.Add(a);
+                    tokens.Add(float.Parse(a));
                 }
                 else
                 {
                     listNum.Add(a);
+                    tokens.Add(float.Parse(a));
+                    tokens.Add(userStr[i].ToString());
                     a = "";
                 }
             }
             var resultOp = String.Join(", ", listOp.ToArray());
             var resultNum = String.Join(", ", listNum.ToArray());
+            var resultTokens = String.Join(", ", tokens.ToArray());
             Console.WriteLine(resultOp);
             Console.WriteLine(resultNum);
-            Console.WriteLine(Calculate(listNum, listOp));
+            Console.WriteLine(resultTokens);
+            RPNImport(tokens);
+            
+            //Console.WriteLine(Calculate(listNum, listOp));
             Console.ReadLine();
         }
-        static double Calculate(List<string> listNum, List<string> listOp)
+        static List<object> RPNImport(List<object> tokens)
         {
-            double result = Convert.ToDouble(listNum[0]);
-            for (int i = 0; i < listOp.Count; i++)
+            Dictionary<string, int> OperationPriority = new Dictionary<string, int>()
             {
-                if (listOp[i] == "*")
+                {"+", 2 },
+                {"-", 2 },
+                {"*", 3 },
+                {"/", 3 },
+                {"^", 4 },
+                {"(", 0 },
+                {")", 1 }
+            };
+            List<object> RPN = new List<object>();
+            Stack<object> stackForOp = new Stack<object>();
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                if (float.TryParse(tokens[i].ToString(), out float n) == true)
                 {
-                    string interimCalc = (Convert.ToDouble(listNum[i]) * Convert.ToDouble(listNum[i + 1])).ToString();
-                    i = DeleteNumOp(listNum, listOp, i, interimCalc);
+                    RPN.Add(tokens[i]);
                 }
-                else if (listOp[i] == "/")
+                else if (float.TryParse(tokens[i].ToString(),out float n1) == false)
                 {
-                    string interimCalc = (Convert.ToDouble(listNum[i]) / Convert.ToDouble(listNum[i + 1])).ToString();
-                    i = DeleteNumOp(listNum, listOp, i, interimCalc);
+                    if (stackForOp.Count == 0)
+                    {
+                        stackForOp.Push(tokens[i]);
+                    }
+                    else if (OperationPriority[tokens[i].ToString()] > OperationPriority[stackForOp.Peek().ToString()])
+                    {
+                        stackForOp.Push(tokens[i]);
+                    }
+                    else if(OperationPriority[tokens[i].ToString()] == 1)
+                    {
+                        while (OperationPriority[stackForOp.Peek().ToString()] > 0)
+                        {
+                            RPN.Add(stackForOp.Pop());
+                        }
+                        stackForOp.Pop();
+                    }
+                    else if (OperationPriority[tokens[i].ToString()] <= OperationPriority[stackForOp.Peek().ToString()])
+                    {
+                        RPN.Add(stackForOp.Pop());
+                        stackForOp.Push(tokens[i]);
+                    }
                 }
             }
-            if (listOp.Count == 0) return Convert.ToDouble(listNum[0]);
-            else
+            if (stackForOp.Count > 0)
             {
-                for (int i = 0; i < listOp.Count; i++)
+                while (stackForOp.Count != 0)
                 {
-                    if (listOp[i] == "+")
-                    {
-                        result += Convert.ToDouble(listNum[i + 1]);
-                    }
-                    else if (listOp[i] == "-")
-                    {
-                        result -= Convert.ToDouble(listNum[i + 1]);
-                    }
+                    RPN.Add(stackForOp.Pop());
                 }
-                return result;
             }
+            var resultRpn = String.Join(", ", RPN.ToArray());
+            Console.WriteLine(resultRpn);
+            return RPN;
+            
         }
 
-        private static int DeleteNumOp(List<string> listNum, List<string> listOp, int i, string interimCalc)
-        {
-            listNum.Insert(i, interimCalc);
-            listNum.RemoveAt(i + 1);
-            listNum.RemoveAt(i + 1);
-            listOp.RemoveAt(i);
-            i--;
-            return i;
-        }
+        //static double Calculate(List<string> listNum, List<string> listOp)
+        //{
+        //    double result = Convert.ToDouble(listNum[0]);
+        //    for (int i = 0; i < listOp.Count; i++)
+        //    {
+        //        if (listOp[i] == "*")
+        //        {
+        //            string interimCalc = (Convert.ToDouble(listNum[i]) * Convert.ToDouble(listNum[i + 1])).ToString();
+        //            i = DeleteNumOp(listNum, listOp, i, interimCalc);
+        //        }
+        //        else if (listOp[i] == "/")
+        //        {
+        //            string interimCalc = (Convert.ToDouble(listNum[i]) / Convert.ToDouble(listNum[i + 1])).ToString();
+        //            i = DeleteNumOp(listNum, listOp, i, interimCalc);
+        //        }
+        //    }
+        //    if (listOp.Count == 0) return Convert.ToDouble(listNum[0]);
+        //    else
+        //    {
+        //        for (int i = 0; i < listOp.Count; i++)
+        //        {
+        //            if (listOp[i] == "+")
+        //            {
+        //                result += Convert.ToDouble(listNum[i + 1]);
+        //            }
+        //            else if (listOp[i] == "-")
+        //            {
+        //                result -= Convert.ToDouble(listNum[i + 1]);
+        //            }
+        //        }
+        //        return result;
+        //    }
+        //}
+
+        //private static int DeleteNumOp(List<string> listNum, List<string> listOp, int i, string interimCalc)
+        //{
+        //    listNum.Insert(i, interimCalc);
+        //    listNum.RemoveAt(i + 1);
+        //    listNum.RemoveAt(i + 1);
+        //    listOp.RemoveAt(i);
+        //    i--;
+        //    return i;
+        //}
     }
 }
