f552fc120d58567f3546c2dcff2d2e8703453fca
DaniilPrikhoddko <d1912dd@gmail.com>
2023-11-05 20:42:07 +0300

    Считает числа любой длины

--- Калькулятор/Калькулятор/Program.cs
+++ Калькулятор/Калькулятор/Program.cs
@@ -10,42 +10,76 @@ namespace калькулятор
     {
         static void Main(string[] args)
         {
+            string a = "";
             string str = Console.ReadLine();
             str = str.Replace(" ", "");
             string operators = "+-*/";
-            List<char> listNum = new List<char>();
-            List<char> listOp = new List<char>();
+            List<string> listNum = new List<string>();
+            List<string> listOp = new List<string>();
             for (int i = 0; i < str.Length; i++)
             {
-                if (operators.Contains(str[i])) listOp.Add(str[i]);
-                else listNum.Add(str[i]);
+                if (operators.Contains(str[i])) listOp.Add(str[i].ToString());
+            }
+
+            for (int i = 0; i < str.Length; i++)
+            {
+
+                if (Char.IsDigit(str[i]) && (i != (str.Length - 1)))
+                {
+                    a += str[i].ToString();
+                }
+                else if (i == (str.Length - 1))
+                {
+                    a += str[i].ToString();
+                    listNum.Add(a);
+                }
+                else
+                {
+                    listNum.Add(a);
+                    a = "";
+                }
             }
             var resultOp = String.Join(", ", listOp.ToArray());
             var resultNum = String.Join(", ", listNum.ToArray());
             Console.WriteLine(resultOp);
             Console.WriteLine(resultNum);
-            Console.WriteLine(Calculate(str));
+            Console.WriteLine(Calculate(listNum, listOp));
         }
-        static int Calculate(string str)
+        static double Calculate(List<string> listNum, List<string> listOp)
         {
-            string a = str;
-            int result = 0;
-            for (int i = 1; i < str.Length; i++)
+            double result = Convert.ToDouble(listNum[0]);
+            for (int i = 0; i < listOp.Count; i++)
             {
-                if ((str[i] == '*') || (str[i] == '/'))
+                if (listOp[i] == "*")
                 {
-                    a = a.Replace(str[i - 1], "").Replace(str[i + 1], "");
-                    if (str[i] == '*') a = a.Replace(str[i].ToString(), Convert.ToString(Convert.ToInt32(str[i - 1]) * Convert.ToInt32(str[i + 1])));
-                    else a = a.Replace(str[i].ToString(), Convert.ToString(Convert.ToInt32(str[i - 1]) / Convert.ToInt32(str[i + 1])));
+                    string interimCalc = (Convert.ToDouble(listNum[i]) * Convert.ToDouble(listNum[i + 1])).ToString();
+                    listNum.Insert(i, interimCalc);
+                    listNum.RemoveAt(i + 1);
+                    listNum.RemoveAt(i + 1);
+                    //listOp.RemoveAt(i);
+                }
+                else if (listOp[i] == "/")
+                {
+                    string interimCalc = (Convert.ToDouble(listNum[i]) / Convert.ToDouble(listNum[i + 1])).ToString();
+                    listNum.Insert(i, interimCalc);
+                    listNum.RemoveAt(i + 1);
+                    listNum.RemoveAt(i + 1);
+                    //listOp.RemoveAt(i);
                 }
             }
-            for (int i = 1; i < a.Length; i++)
+            listOp.Remove("*");
+            listOp.Remove("/");
+            for (int i = 0; i < listOp.Count; i++)
             {
-                if ((a[i] == '+') || (a[i] == '-'))
+                if (listOp[i] == "+")
                 {
-                    if (a[i] == '+') result += Convert.ToInt32(a[i - 1]) + Convert.ToInt32(a[i + 1]);
-                    else result += Convert.ToInt32(a[i - 1]) - Convert.ToInt32(a[i + 1]);
+                    result += Convert.ToDouble(listNum[i + 1]);
                 }
+                else if (listOp[i] == "-")
+                {
+                    result -= Convert.ToDouble(listNum[i + 1]);
+                }
+
             }
             return result;
         }
